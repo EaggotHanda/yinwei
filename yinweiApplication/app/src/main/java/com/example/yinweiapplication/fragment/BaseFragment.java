6e57bc157a8f07631416d8fa1c1ebebc9862f015
EaggotHanda <yixue1985910u@126.com>
2021-05-21 14:15:37 +0800

    test

--- yinweiApplication/app/src/main/java/com/example/yinweiapplication/fragment/BaseFragment.java
+++
@@ -1,9 +0,0 @@
-package com.example.yinweiapplication.fragment;
-
-import android.support.v4.app.Fragment;
-
-/**
- * Created by 刘鹏 on 2015/7/5.
- */
-public class BaseFragment extends Fragment {
-}
--- yinweiApplication/app/src/main/java/com/example/yinweiapplication/fragment/MyFragment.java
+++
@@ -1,7 +0,0 @@
-package com.example.yinweiapplication.fragment;
-
-/**
- * Created by 刘鹏 on 2015/7/5.
- */
-public class MyFragment extends BaseFragment {
-}
